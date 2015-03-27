16acc2b3607b057868357e00039e92cd068d46bc
Christian Wahl <chrwahl@gmail.com>
2015-03-27 11:49:10 +0100

    added session06

---
+++ session06/live/example.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '192.168.89.128');
+
+console.log('Server running at http://192.168.89.128:1337/');
